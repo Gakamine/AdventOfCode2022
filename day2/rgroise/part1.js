c89b0e5be3d9aeee2f20fb253bc6b2ec3cdef211
rgroise <rgroise@gmail.com>
2022-12-05 11:15:13 +0100

    more consistnet quote-ations

--- day2/rgroise/part1.js
+++ day2/rgroise/part1.js
@@ -1,5 +1,5 @@
 console.log(require('fs').readFileSync('input', 'utf-8').split(/\r?\n/).filter(l => !!l)
-    .map(line => line.split(" "))
+    .map(line => line.split(' '))
     .map(([left, right]) => ([left.charCodeAt(0) - 65, right.charCodeAt(0) - 88]))
     .map(([left, right]) => 
         right + 1 + (
