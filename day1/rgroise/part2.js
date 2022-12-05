21ce66165d17f4a49eb7a37fb15578ced8ee058f
rgroise <rgroise@gmail.com>
2022-12-05 11:15:28 +0100

    js-er type casting

--- day1/rgroise/part2.js
+++ day1/rgroise/part2.js
@@ -1,6 +1,6 @@
 console.log(require('fs').readFileSync('input', 'utf-8').split(/\r?\n/).reduce((a, b) => {
     if(!!b) {
-        a[a.length - 1] += parseInt(b)
+        a[a.length - 1] += +b
         return a
     }
     return [...a, 0]
