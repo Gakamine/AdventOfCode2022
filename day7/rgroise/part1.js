0134449b9b0b95c499cffdf844f3644f73dd9eb5
rgroise <rgroise@gmail.com>
2022-12-07 23:34:20 +0100

    adv day7

--- day7/rgroise/part1.js
+++ day7/rgroise/part1.js
@@ -1,38 +1,34 @@
-(async() => {
+(async() => { //this is probably about the dumbest way to do this, but it was fun
+    const isWin = process.platform.startsWith('win'), //platform agnosticism XD
+     tmpdir = 'tmp7'
     let basepath = process.cwd()
-    const shell = require('child_process').spawn(process.platform.startsWith('win') ? 'powershell' : 'bash', {cwd: basepath})
-    shell.on('error', (err) => console.log(err))  
-    const shellExec = async(command) => {
+    const shell = require('child_process').spawn(isWin ? 'cmd' : 'bash', {cwd: basepath}),
+     shellExec = async(command) => {
         await new Promise((resolve, _) => {
             if (!shell.stdin.write(command+'\r\n')) {
-                shell.once('drain', async () => {
-                    await shellExec(command)
-                    resolve()
-                })
-            }
-            process.nextTick(resolve)
-        })
-    }
-    await shellExec('mkdir day7tmp')
-    await shellExec('cd day7tmp')
-    let txt = require('fs').readFileSync('../example', 'utf-8').replaceAll('dir', '$ mkdir')
-    if(process.platform.startsWith('win')){
-        txt = txt.replaceAll('cd ', 'cd ./') //WHY WONT YOU WORK GODDAMNIT REEEEEEEE
-    }
-    txt.split(/\r?\n/).filter(l => !!l && l !='$ ls').slice(1).map(async l => {
+                 shell.once('drain', async () => {
+                     resolve(await shellExec(command))      
+                 })
+             }
+             process.nextTick(resolve)
+         })
+     }
+    await shellExec(`mkdir ${tmpdir}`)
+    await shellExec(`cd ${tmpdir}`)
+    let txt = require('fs').readFileSync('../example', 'utf-8').replaceAll('dir', '$ mkdir').split(/\r?\n/).filter(l => !!l && l !='$ ls')
+    for(i = 1 ; i < txt.length; i++) {
+        let l = txt[i]
         if(l[0] == '$') {
-            console.log(l.slice(2))
             await shellExec(l.slice(2))
         }
         else {
             [size, file] = l.split(' ')
-            console.log(size, file)
-            require('fs').writeFileSync('./'+file, ' '.repeat(size))
+            await shellExec(isWin ? `fsutil file createnew ${file} ${size}` : `truncate -s ${file} ${size}`)
         }
-    })
+    }
 
-    shell.kill('SIGINT')
-    process.exit(0)
+    //todo file scan/result
 
-    // require('child_process').run('rm -rf', {cwd: basepath}) //cleanup
+    await shellExec(isWin ? `rd /s/q ${tmpdir}` : `rm -rf ${tmpdir}`)
+    await shellExec('exit')
 })()
