69a4d2e4d992450ae52496623fc995cacec7b898
rgroise <rgroise@gmail.com>
2022-12-09 22:52:45 +0100

    The Importance of Taking Things One Step at a Time

--- day9/rgroise/part1.js
+++ day9/rgroise/part1.js
@@ -2,13 +2,13 @@ let headPos = [0,0],
  tailPos = [0,0],
  visited = [[0,0]]
 require('fs').readFileSync('input', 'utf-8').split(/\r?\n/)
-    .map(([direction, _,...displacement]) => {
-        displacement = displacement.join('')
+    .map(([direction, _, ...displacement]) => {
+        displacement = +displacement.join('')
         switch(direction){
-            case 'R': headPos[0] += +displacement ; break
-            case 'L': headPos[0] -= +displacement ; break
-            case 'U': headPos[1] += +displacement ; break
-            case 'D': headPos[1] -= +displacement ; break
+            case 'R': headPos[0] += displacement ; break
+            case 'L': headPos[0] -= displacement ; break
+            case 'U': headPos[1] += displacement ; break
+            case 'D': headPos[1] -= displacement ; break
         }
         let xDiff = headPos[0] - tailPos[0],
         yDiff = headPos[1] - tailPos[1]
