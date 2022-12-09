eb5f3d36e7ad2261afc72b3869d9ca9eee9da60e
rgroise <rgroise@gmail.com>
2022-12-09 20:44:01 +0100

    IT HAPPENED AGAIN WHY IM GOING INSANE

---
+++ day9/rgroise/part2.js
@@ -0,0 +1,28 @@
+let headPos = [0,0],
+ knotPositions = Array(9).fill().map((e) => e = [0,0])
+ visited = [[0,0]]
+require('fs').readFileSync('../example2', 'utf-8').split(/\r?\n/)
+    .map(([direction, _, ...displacement]) => {
+        displacement = displacement.join('')
+        switch(direction){
+            case 'R': headPos[0] += +displacement ; break
+            case 'L': headPos[0] -= +displacement ; break
+            case 'U': headPos[1] += +displacement ; break
+            case 'D': headPos[1] -= +displacement ; break
+        }
+        knotPositions.map((knotPos, i) => {
+            let prevKnotPos = (i == 0 ? headPos : knotPositions[i-1]),
+             xDiff = prevKnotPos[0] - knotPos[0],
+             yDiff = prevKnotPos[1] - knotPos[1]
+            while(Math.abs(xDiff) > 1 || Math.abs(yDiff) > 1){
+                knotPos[0] += xDiff != 0 ? xDiff/Math.abs(xDiff) : 0
+                knotPos[1] += yDiff != 0 ? yDiff/Math.abs(yDiff) : 0
+                if(i == knotPositions.length - 1 &&
+                    visited.findIndex(([x, y]) => x == knotPos[0] && y == knotPos[1]) < 0
+                ) visited.push([...knotPos])
+                xDiff = prevKnotPos[0] - knotPos[0]
+                yDiff = prevKnotPos[1] - knotPos[1]
+            }  
+        })
+    })
+console.log(visited.length)
