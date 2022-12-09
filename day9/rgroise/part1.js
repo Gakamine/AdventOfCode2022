012cb4c8ac259abd0e936a8e6c5489ebe2383330
rgroise <rgroise@gmail.com>
2022-12-09 19:34:08 +0100

    day9 part 1 (somehow breaks wth actual input)

---
+++ day9/rgroise/part1.js
@@ -0,0 +1,24 @@
+let headPos = [0,0],
+ tailPos = [0,0],
+ visited = [[0,0]]
+require('fs').readFileSync('../example', 'utf-8').split(/\r?\n/)
+    .map(([direction, _,displacement]) => {
+        switch(direction){
+            case 'R': headPos[0] += +displacement ; break
+            case 'L': headPos[0] -= +displacement ; break
+            case 'U': headPos[1] += +displacement ; break
+            case 'D': headPos[1] -= +displacement ; break
+        }
+        let xDiff = headPos[0] - tailPos[0],
+        yDiff = headPos[1] - tailPos[1]
+        // console.log(headPos)
+        while(Math.abs(xDiff) > 1 || Math.abs(yDiff) > 1){
+            tailPos[0] += xDiff != 0 ? xDiff/Math.abs(xDiff) : 0
+            tailPos[1] += yDiff != 0 ? yDiff/Math.abs(yDiff) : 0
+            if(visited.findIndex(([x, y]) => x == tailPos[0] && y == tailPos[1]) < 0) visited.push([...tailPos])
+            // console.log('--', tailPos)
+            xDiff = headPos[0] - tailPos[0]
+            yDiff = headPos[1] - tailPos[1]
+        }  
+    })
+console.log(visited.length)
