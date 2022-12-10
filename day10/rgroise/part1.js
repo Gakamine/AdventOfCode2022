d6ea594fe9f13910fd38e849ed7ba682a857b981
rgroise <remi.groise@efrei.net>
2022-12-10 14:25:08 +0100

    bruh

--- day10/rgroise/part1.js
+++ day10/rgroise/part1.js
@@ -1,14 +1,7 @@
 let i = X = 1
-console.log(require('fs').readFileSync('../example', 'utf-8').split(/\r?\n/).map(l => l.split(' '))
+console.log(require('fs').readFileSync('input', 'utf-8').replaceAll('addx', 'noop \naddx').split(/\r?\n/).map(l => l.split(' '))
     .reduce((a, [opcode, val] ) => {
-        if(opcode == 'addx') {
-            i++
-            if((i-20) % 40 == 0) {
-                console.log(i, X, X * i)
-                return a + X * i
-            }
-            X += +val
-        }
+        if(opcode == 'addx') X += +val
         i ++
         if((i-20) % 40 == 0) {
             console.log(i, X, X * i)
