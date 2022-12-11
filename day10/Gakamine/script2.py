09a8607aa0fc1a37f539b750fb370d93e18d3fae
Gakamine <jolyantoine@pm.me>
2022-12-11 17:39:20 -0500

    Day10 part1

---
+++ day10/Gakamine/script2.py
@@ -0,0 +1,19 @@
+file = open("input", "r")
+x=1
+cycle=1
+
+def clock_tick(current_cycle,signal,x):
+    cycles=[20,60,100,140,180,220]
+    if current_cycle in cycles:
+        signal+=(x*cycle)
+    return (current_cycle+1,signal)
+
+for line in file:
+    line=line.strip().split(" ")
+    if line[0]=="noop":
+        cycle,signal=clock_tick(cycle,signal,x)
+    elif line[0]=="addx":
+        cycle,signal=clock_tick(cycle,signal,x)
+        cycle,signal=clock_tick(cycle,signal,x)
+        x+=int(line[1])
+
